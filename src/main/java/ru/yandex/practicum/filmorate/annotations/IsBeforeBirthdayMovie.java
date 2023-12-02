f3ec0268047a976fd8b5f9e3887ecdf92bd061b8
DmitriyDemintsev <d.a.demintsev@yandex.ru>
2023-12-02 23:08:01 +0300

    поправил стайт-тест

--- src/main/java/ru/yandex/practicum/filmorate/annotations/IsBeforeBirthdayMovie.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotations/IsBeforeBirthdayMovie.java
@@ -1,13 +1,11 @@
 package ru.yandex.practicum.filmorate.annotations;
 
 import javax.validation.Constraint;
-import javax.validation.constraints.Past;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = IsBeforeBirthdayMovieValidator.class)
-//@Past
 public @interface IsBeforeBirthdayMovie {
     String message() default "Дата должна быть не раньше {value}";
 
