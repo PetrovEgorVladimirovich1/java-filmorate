9587903fb028964c271ad06830dd53034ba0a5ae
Aleksey01091993 <AShvedov1993@yandex.ru>
2023-12-04 21:05:43 +0300

    new

--- src/main/java/ru/yandex/practicum/filmorate/storage/dal/ReviewsStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dal/ReviewsStorage.java
@@ -5,13 +5,21 @@ import ru.yandex.practicum.filmorate.model.Reviews;
 import java.util.List;
 
 public interface ReviewsStorage {
-    Reviews create (Reviews reviews);
+    Reviews create(Reviews reviews);
+
     Reviews update(Reviews reviews);
+
     Reviews remove(Long id);
+
     Reviews get(Long id);
+
     List<Reviews> getReviewsFilm(Long id, Long count);
+
     Reviews addLike(Long id, Long userId);
+
     Reviews addDislike(Long id, Long userId);
+
     Reviews removeLike(Long id, Long userId);
+
     Reviews removeDislike(Long id, Long userId);
 }
